11ad9080056fc4b4b188b7a914f25de31cac9c8b
Apple <apple@apples-mbp-3.home>
2018-07-31 12:44:07 +0100

    initial commit

---
+++ lesson1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        for(int i = 15; i >= 1; i--) {
+            System.out.println(i + " Java");
+        }
+        int i = 0;
+        do {
+            System.out.println(i + " Java");
+            i++;
+        } while (i <= 15);
+    }
+}
